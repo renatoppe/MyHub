8fdc6f023e834db6d052dc0461188418290a2f2c
renatoppe <124149010+renatoppe@users.noreply.github.com>
2025-12-04 19:26:25 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "myhub-v0.3";
+const CACHE_NAME = "myhub-v0.4";
 const URLS_TO_CACHE = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
@@ -26,3 +26,4 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
